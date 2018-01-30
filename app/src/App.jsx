e8b0cf0dd7b476155f0247378b3928cd9988761c
Joseph Surin <joseph.surin@gmail.com>
2018-01-30 17:00:30 +0800

    video setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-require("./styles/main.sass");
+require('./styles/main.sass')
 
-import MyComponent from './components/MyComponent.jsx';
-
-const reactLogo = require('./assets/react.png');
+import AnimeVideo from './components/AnimeVideo.jsx'
 
 export default class App extends Component {
-    render() {
-        return(
-            <div>
-                <div className="title"> Hello </div>
-
-                <img src= {reactLogo} />
-
-                <MyComponent dummyProp="random!" />
-
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+			
+			</div>
+		)
+	}
 }
--- app/src/components/MyComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class MyComponent extends Component { 
-    render() {
-        return (
-            <h2> this is very {this.props.dummyProp} </h2>
-        );
-    }
-}
