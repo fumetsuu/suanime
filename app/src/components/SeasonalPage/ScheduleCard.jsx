1108824226dc7483056cb45f6f1ba0788fa6c4b8
Joseph Surin <joseph.surin@gmail.com>
2018-03-13 17:20:22 +0800

    schedule card click to info page

--- app/src/components/SeasonalPage/ScheduleCard.jsx
+++ app/src/components/SeasonalPage/ScheduleCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { toAMPM, fixTzOffset, dayofweekFromDayandTime } from '../../util/util.js'
+import { launchInfo } from '../../actions/actions.js'
 
 const ScheduleCard = (cardData) => (
-    <div className="schedule-card">
+    <div className="schedule-card" onClick={() => {launchInfo(cardData.cardData.anime.title, cardData.cardData.anime.slug, cardData.cardData.anime.id)}}>
         <div className="bg-img" style={{ backgroundImage: `url('https://cdn.masterani.me/poster/${cardData.cardData.anime.poster.file}')`}}/>
         <div className="broadcast-time">{cardData.cardData.release_time ? (((new Date().getHours()+new Date().getMinutes() / 60) > fixTzOffset(cardData.cardData.release_time).timeInHours && dayofweekFromDayandTime(cardData.cardData.day_of_week, cardData.cardData.release_time || '?') == new Date().getDay()) ? 'Ep. '+cardData.cardData.latest_released_episode.episode+' released' : (toAMPM(fixTzOffset(cardData.cardData.release_time)))+' - Ep. '+cardData.cardData.next_episode) : '?'}</div>
         <div className="anime-title">{cardData.cardData.anime.title}</div>
     </div>
-);
+)
 
-export default ScheduleCard;
+export default ScheduleCard
