e7ec28d7f75eb69a3bd5c5ecabac0441495a0c9b
Joseph Surin <joseph.surin@gmail.com>
2018-08-21 17:39:27 +0800

    fix downloads (masterani HTML change) + use local mp4upload scraper, fix rapidvideo scraper

--- app/src/util/getDownloadLink.js
+++ app/src/util/getDownloadLink.js
@@ -5,26 +5,24 @@ export function getDownloadLink(epLink, getHD) {
     return new Promise((resolve, reject) => {
         rp(epLink)
         .then(body => {
-            var streamdataformat = /var args = (.*)/g
-            var match = streamdataformat.exec(body)
-            var streamdata = JSON.parse(
-                match[0]
-                    .split('args = ')[1]
-                    .replace(/anime: {/, '"anime": {')
-                    .replace(/mirrors: \[/, '"mirrors": [')
-                    .replace(/auto_update: \[/, '"auto_update": [')
-            )
-
+            var streamdataformat = /:mirrors='(.*)/g
+			var match = streamdataformat.exec(body)
+			var streamdata = { mirrors: 
+				JSON.parse(
+					match[0]
+						.split(':mirrors=\'')[1]
+						.split('\'></video-mirrors>')[0]
+				)
+			}
             var mirrorsPromises = [
 				() => getStreamMoeURL(streamdata),
                 () => getRapidvideoURL(streamdata),
 				() => getStreamangoURL(streamdata),
-                () => getmp4UploadURL(epLink),
+                () => getmp4UploadURL(streamdata),
                 () => getAikaURL(body)
             ]
 
             pAny(mirrorsPromises).then(link => {
-                console.log(link)
                 return resolve(link)
             }).catch(err => reject(err))
 
@@ -38,7 +36,7 @@ export function getDownloadLink(epLink, getHD) {
             } else {
                 var workingMirror = streamdata.mirrors.reverse().find(mirror => mirror.host.id == 19)
             }
-            if(!workingMirror) return reject('ERR 404')
+            if(!workingMirror) return reject('ERR 404 StreamMoe')
             var embedURL = 'https://stream.moe/' + workingMirror.embed_id
             rp(embedURL).then(body => {
 				var linkRegex = /<a href="(.*)" target="_blank">\(download\)<\/a>/
@@ -48,26 +46,25 @@ export function getDownloadLink(epLink, getHD) {
         })
     }
 
-    function getmp4UploadURL(epLink) {
+    function getmp4UploadURL(streamdata) {
         return new Promise((resolve, reject) => {
-            var slug = epLink.split("watch/")[1].split("/")[0]
-            var epNumber = epLink.split("watch/")[1].split("/")[1]
-            var corsageURL = `https://corsage-sayonara.herokuapp.com/masterani/api/video/?slug=${slug}&ep=${epNumber}`
-            rp({ uri: corsageURL, json: true }).then(mirrorsArray => {
-                if(getHD) {
-                    var workingMirror = mirrorsArray.reverse().find(mirror => mirror.id == 1)
-                } else {
-                    var workingMirror = mirrorsArray.find(mirror => mirror.id == 1)                    
-                }
-                if(workingMirror) {
-                    var downloadURL = workingMirror.link
-                    return resolve(downloadURL)
-                }
-                return reject('ERR 404')
-            }).catch(err => {
-                console.log('corsage-sayonara api error', err)
-                return reject(err)
-            })
+			if(getHD) {
+				var workingMirror = streamdata.mirrors.find(mirror => mirror.host.id == 1)
+			} else {
+				var workingMirror = streamdata.mirrors.reverse().find(mirror => mirror.host.id == 1)
+			}
+			if(!workingMirror) return reject('ERR 404 MP4UPLOAD')
+			var embedURL = 'https://mp4upload.com/embed-' + workingMirror.embed_id + '.html'
+			rp(embedURL).then(body => {
+				var evalPackedFunctionRegex = /eval\((.*)\)/g
+				var evalPackedFunction = evalPackedFunctionRegex.exec(body)[0]
+				eval(evalPackedFunction)
+				if(player.info.file) {
+					return resolve(player.info.file)
+				} else {
+					return reject('ERR 404 MP4UPLOAD')
+				}
+			})
         })
     }
 
@@ -75,7 +72,7 @@ export function getDownloadLink(epLink, getHD) {
         return new Promise((resolve, reject) => {
             var videosformat = /var videos = \[(.*)\]/g
             var match = videosformat.exec(pagehtml)
-            if(!match) return reject('ERR 404')
+            if(!match) return reject('ERR 404 Aika')
             var videosdata = JSON.parse( //array
                 '['+match[1]+']'
             )
@@ -101,24 +98,11 @@ export function getDownloadLink(epLink, getHD) {
     function getRapidvideoURL(streamdata) {
         return new Promise((resolve, reject) => {
             var workingMirror = streamdata.mirrors.find(mirror => mirror.host.id == 21)
-            if(!workingMirror) return reject('ERR 404')
-            var embedURL = 'https://www.rapidvideo.com/e/' + workingMirror.embed_id
+            if(!workingMirror) return reject('ERR 404 RapidVideo')
+            var embedURL = 'https://www.rapidvideo.com/?v=' + workingMirror.embed_id
             rp(embedURL).then(body => {
 				var linkRegex = /<source src="(.*)\.mp4"/
-				var linkRegex2 = /src: "(.*)\.mp4"/
-				if(!linkRegex.test(body) && !linkRegex2.test(body)) return reject('ERR 404')
-				var link = ''
-				if(linkRegex.test(body)) {
-					link = linkRegex.exec(body)[1] + '.mp4'
-				} else {
-					link = linkRegex2.exec(body)[1] + '.mp4'
-				}
-                // var linkRegex = /<a href="(.*)" id="button-download"/g
-                // var links = []
-                // while((res = linkRegex.exec(body)) !== null) {
-                //     links.push(res[1])
-                // }
-                // if(getHD) return resolve(links[links.length-1])
+				var link = linkRegex.exec(body)[0].split('<source src="')[1].split('"')[0]
 				return resolve(link)
             })
         })
@@ -131,13 +115,12 @@ export function getDownloadLink(epLink, getHD) {
             } else {
                 var workingMirror = streamdata.mirrors.reverse().find(mirror => mirror.host.id == 18)
             }
-            if(!workingMirror) return reject('ERR 404')
+            if(!workingMirror) return reject('ERR 404 Streammango')
             var embedURL = 'https://streamango.com/embed/' + workingMirror.embed_id
             rp(embedURL).then(body => {
 				var linkRegex = /src:d\((.*)\),/
 				var rawRes = linkRegex.exec(body)[1].replace(/'/g, '"')
 				var packedData = JSON.parse(`[${rawRes}]`)
-				console.log(packedData)
 				var downloadURL = 'https:'+unpackStreamango(...packedData)
 				return resolve(downloadURL)
             })
@@ -153,7 +136,7 @@ function pAny(promiseArray) {
 		var cn = 0
 		var errors = []
 		const exec = index => {
-			if(index == len-1) return reject(new AggregateError(errors))
+			if(index == len) reject(new AggregateError(errors))
 			promiseArray[index]().then(
 				resolve,
 				err => { 
@@ -165,4 +148,10 @@ function pAny(promiseArray) {
 		}
 		exec(cn)
 	})
+}
+
+/* mp4upload stuff, the mp4upload eval js will put the info into a variable named player, the data will be available through player.info */
+var jwplayer = function(a) {
+	this.info = {}
+	return { setup: function(info) { this.info = info }, addButton: function() {}, on: function() {} }
 }
